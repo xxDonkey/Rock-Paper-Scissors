e1150691cf904b6a685c926aa8d10352900dd1d6
Dylan R <d.raiff@icloud.com>
2022-01-12 07:16:39 -0500

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hard_toggle = document.getElementById("hard");
 
+// Define a dictionary that maps moves to their winning moves.
 const winning = {
     "r": "p",
     "p": "s",
@@ -9,8 +10,10 @@ const winning = {
 var hard = false;
 var bo3 = false;
 
+// Link the toggle button to this script
 hard_toggle.onclick = () => { hard = hard_toggle.checked; };
 
+// Convert a letter representation to its corresponding word
 const repr_to_word = (repr) =>
 {
     switch (repr)
@@ -24,20 +27,26 @@ const repr_to_word = (repr) =>
     }
 }
 
+// Called on any move button press
 const player_move = (move) =>
 {
+    // Generate a random move for the computer
     let computer_move = ["r", "p", "s"][Math.floor(Math.random() * 3)];
-    if (hard && Math.random() > 0.8)
+
+    // If we are on hard, we make the computer win 80% of the time
+    if (hard && Math.random() > 0.2)
     {
         computer_move = winning[move];
     }
     
+    // Check who is the winner
     let winner = "The Player wins!";
     if (computer_move == winning[move])
         winner = "The Computer wins!";
     else if (computer_move == move)
         winner = "It's a tie."
         
+    // Display on webpage
     document.getElementById("out").innerHTML = "You played \"" + repr_to_word(move) 
         + "\" and the computer played \"" + repr_to_word(computer_move) + "\". " + winner;
 }
